e679dc97dc45155a4da176640edc47f09d69133c
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2024-08-09 11:22:03 +0700

    fix modal

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// Click vào nút hiệu chỉnh xuất hiện màn hình mới 
+// Click vào nút hiệu chỉnh xuất hiện màn hình mới
 document.addEventListener("DOMContentLoaded", () => {
-    const modal = document.getElementById("modal");
-    const openModalButtons = document.querySelectorAll(".openModal");
-    const closeButton = document.querySelector(".close-button");
+  const modal = document.getElementById("modal");
+  const openModalButtons = document.querySelectorAll(".openModal");
+  const closeButton = document.querySelector(".close-button");
 
-    openModalButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            modal.style.display = "block";
-        });
+  openModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      modal.style.display = "block";
     });
+  });
 
-    closeButton.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
+  closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
 
-    window.addEventListener("click", (event) => {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
 });
