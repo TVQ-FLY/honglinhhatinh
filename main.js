bc5f2827d1c0e1c002b6a2c44218d8fbc2d2ae5e
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2024-08-09 10:37:52 +0700

    fix view hieu chinh

---
+++ main.js
@@ -0,0 +1,22 @@
+// Click vào nút hiệu chỉnh xuất hiện màn hình mới 
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("modal");
+    const openModalButtons = document.querySelectorAll(".openModal");
+    const closeButton = document.querySelector(".close-button");
+
+    openModalButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            modal.style.display = "block";
+        });
+    });
+
+    closeButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
